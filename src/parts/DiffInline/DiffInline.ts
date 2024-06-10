120ec342a30e39d65852fa5df7f4a0db79dfea57
Le Vivilet <levivlet@pm.me>
2024-06-10 07:26:07 +0200

    feature: add inline diff function

---
+++ src/parts/DiffInline/DiffInline.ts
@@ -0,0 +1,46 @@
+import * as Diff from '../Diff/Diff.ts'
+import * as DiffType from '../DiffType/DiffType.ts'
+import type { InlineDiffItem } from '../InlineDiffItem/InlineDiffItem.ts'
+
+export const diffInline = (linesLeft: readonly string[], linesRight: readonly string[]): readonly InlineDiffItem[] => {
+  const { changesLeft, changesRight } = Diff.diff(linesLeft, linesRight)
+  const lengthLeft = linesLeft.length
+  const lengthRight = linesRight.length
+  const merged: InlineDiffItem[] = []
+  let leftIndex = 0
+  let rightIndex = 0
+  while (leftIndex < lengthLeft && rightIndex < lengthRight) {
+    const left = changesLeft[leftIndex]
+    const right = changesRight[rightIndex]
+    if (left.type === right.type) {
+      merged.push({
+        leftIndex,
+        rightIndex,
+        type: DiffType.None,
+      })
+      leftIndex++
+      rightIndex++
+    } else if (left.type === DiffType.Deletion) {
+      merged.push({
+        leftIndex,
+        rightIndex,
+        type: DiffType.Deletion,
+      })
+      leftIndex++
+    } else if (leftIndex <= rightIndex) {
+      merged.push({
+        leftIndex,
+        rightIndex,
+        type: left.type,
+      })
+      leftIndex++
+    } else if (leftIndex > rightIndex) {
+      merged.push({
+        leftIndex,
+        rightIndex,
+        type: right.type,
+      })
+    }
+  }
+  return merged
+}
