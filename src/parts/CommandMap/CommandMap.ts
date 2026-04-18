465b9dd8f59e34c1d8625f86d67023daea756560
Le Vivilet <levivilet@pm.me>
2026-04-18 21:41:58 +0200

    feature: add handleMessagePort function and update commandMap (#34)
    * feature: add handleMessagePort function and update commandMap
    
    * fix
    
    * fix
    
    * cov

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,9 @@
 import * as Diff from '../Diff/Diff.ts'
 import * as DiffInline from '../DiffInline/DiffInline.ts'
+import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
 
 export const commandMap = {
   'Diff.diff': Diff.diff,
   'Diff.diffInline': DiffInline.diffInline,
+  'Diff.handleMessagePort': handleMessagePort,
 }
